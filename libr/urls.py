0b625697970d531838dd4b941f97a4c2c6d97d3a
ninjamar <70116773+ninjamar@users.noreply.github.com>
2025-01-13 17:42:46 +0800

    Add some more directory structuring

--- libr/urls.py
+++ libr/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -29,3 +32,8 @@ urlpatterns = [
     ),
     path("", include(("info.urls", "info"), namespace="info")),
 ]
+
+urlpatterns += debug_toolbar_urls()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
