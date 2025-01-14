3646f14048f9984ff085da6b857904bc098f49c2
ninjamar <70116773+ninjamar@users.noreply.github.com>
2025-01-14 10:07:30 +0800

    add custom user model

--- user_management/admin.py
+++ user_management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 # Register your models here.
+
+# Register the User model
+admin.site.register(User, UserAdmin)
