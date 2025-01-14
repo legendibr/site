bc1c85a49139a8f1560bac16a218ab371ebca7cb
ninjamar <70116773+ninjamar@users.noreply.github.com>
2025-01-15 07:54:47 +0800

    added burger menu

--- static/base.js
+++ static/base.js
@@ -1,14 +1,46 @@
-let isPopoutMenuOpen = false;
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector("#mobile-navbar-burger");
+    const popup = document.querySelector("#mobile-navbar-popup");
+    
+    const navbar = document.querySelector(".main-navbar");
+    const title = document.querySelector(".title");
+    const links = document.querySelectorAll(".links");
 
-function togglePopoutMenu(){
-    if (isPopoutMenuOpen){
-        // Close it
-        document.querySelector("#popout-menu").style.display = "none";
-        isPopoutMenuOpen = false;
-    } else {
-        // Open it
-        document.querySelector("#popout-menu").style.display = "visible";
-        document.querySelector("#popout-menu").style.animation = "fadeIn 0.5s forwards";
-        isPopoutMenuOpen = true;
-    }
-}
+    let isPopupOpen = false;
+    
+    let closePopup = () => {
+        popup.style.display = "none";
+
+        links[0].classList.add("hide-mobile");
+        links[1].classList.add("hide-mobile");
+
+        navbar.insertBefore(links[0], title);
+        navbar.insertBefore(links[1], title.nextSibling);
+
+        isPopupOpen = false;
+    };
+    /* Move all the links */
+    let openPopup = () => {
+        let frag = document.createDocumentFragment();
+
+        links[0].classList.remove("hide-mobile");
+        links[1].classList.remove("hide-mobile");
+        frag.append(links[0]);
+        frag.append(links[1]);
+
+        popup.appendChild(frag);
+        popup.style.display = "block";
+
+        isPopupOpen = true;
+    };
+
+    burger.addEventListener("click", () => {
+        if (isPopupOpen){
+            // close
+            closePopup();
+        } else {
+            // open
+            openPopup();
+        }
+    });
+});
