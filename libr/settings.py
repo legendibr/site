51db00a12484754028959ff894422eee773116b2
ninjamar <70116773+ninjamar@users.noreply.github.com>
2025-01-18 21:41:09 +0800

    fix issue with loading env

--- libr/settings.py
+++ libr/settings.py
@@ -25,7 +25,7 @@ load_dotenv(BASE_DIR / ".env")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ["DEBUG"])
+DEBUG = os.environ["DEBUG"] == "True" # can't use bool constructor here, instead use equality
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.environ["SECRET_KEY"])
