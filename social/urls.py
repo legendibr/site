e1803ac63b82fe805cb3e0afcf8d3ba70e4e39bc
ninjamar <70116773+ninjamar@users.noreply.github.com>
2025-01-19 15:21:14 +0800

    fix typo

--- social/urls.py
+++ social/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("api/post_comment", views.post_comment, name="api_post_comment"),
-    path("api/get_comments", views.get_all_comments, name="api_get_comments"),
+    path("api/get_comments", views.get_comments, name="api_get_comments"),
     path("test", views.test_comment, name="test"),
 ]
