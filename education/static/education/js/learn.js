37b333ea4912951c956c1fc8e30080869136fac9
ninjamar <70116773+ninjamar@users.noreply.github.com>
2025-01-22 20:44:41 +0800

    update sidebar toggle positioning

--- education/static/education/js/learn.js
+++ education/static/education/js/learn.js
@@ -3,9 +3,11 @@ function toggleSidebar(){
     // could just do .classList.toggle, but idk
     if (isSidebarOpen){
         document.querySelector(".sidebar").classList.remove("active");
+        document.querySelector("#sidebar-toggle").classList.remove("active");
         isSidebarOpen = false;
     } else {
         document.querySelector(".sidebar").classList.add("active");
+        document.querySelector("#sidebar-toggle").classList.add("active");
         isSidebarOpen = true;
     }
 }
