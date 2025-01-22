af538b0de1fd74fe4df1122f2282d7137e9a3177
ninjamar <70116773+ninjamar@users.noreply.github.com>
2025-01-22 20:33:48 +0800

    added toggleable sidebar

--- education/static/education/js/learn.js
+++ education/static/education/js/learn.js
@@ -1,15 +1,11 @@
-/*
-window.onload = function() {
-    const header = document.querySelector('header');
-    const footer = document.querySelector('footer');
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.height = window.innerHeight - header.offsetHeight - footer.offsetHeight + 'px';
-    const content = document.querySelector('.content-container');
-    content.style.width = window.innerWidth - sidebar.offsetWidth + 'px';
-
-    window.onresize = function() {
-        sidebar.style.height = window.innerHeight - header.offsetHeight - footer.offsetHeight + 'px';
-        content.style.width = window.innerWidth - sidebar.offsetWidth + 'px';
+let isSidebarOpen = false
+function toggleSidebar(){
+    // could just do .classList.toggle, but idk
+    if (isSidebarOpen){
+        document.querySelector(".sidebar").classList.remove("active");
+        isSidebarOpen = false;
+    } else {
+        document.querySelector(".sidebar").classList.add("active");
+        isSidebarOpen = true;
     }
 }
-*/
